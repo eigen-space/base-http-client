d503b96c8ebfa186662d0d75916547c4b26b5fda
Nikita Agupov <nktrsk@gmail.com>
2020-11-20 14:20:44 +0300

    #1 common: fixed detecting content type

--- src/entities/request-provider-response/request-provider-response.spec.ts
+++ src/entities/request-provider-response/request-provider-response.spec.ts
@@ -18,6 +18,29 @@ describe('RequestProviderResponse', () => {
             expect(data).toEqual(expectedData);
         });
 
+        it('should return processed json if content type contains not only type', async () => {
+            const json = { data: 'json-data' };
+            const nativeResponse = new NativeResponseStub(json, 'application/json;charset=UTF-8');
+            const response = new RequestProviderResponseStub(nativeResponse);
+
+            const data = await response.data();
+
+            const expectedData = { data: 'json-data' };
+            expect(data).toEqual(expectedData);
+        });
+
+        it('should throw error for empty content type', async () => {
+            const rawData = 'plain-text';
+            const nativeResponse = new NativeResponseStub(rawData, undefined);
+            const response = new RequestProviderResponseStub(nativeResponse);
+
+            try {
+                await response.data();
+            } catch (e) {
+                expect(e).toBeDefined();
+            }
+        });
+
         it('should throw error for unprocessed response', async () => {
             const rawData = 'plain-text';
             const nativeResponse = new NativeResponseStub(rawData, ContentType.PLAIN_TEXT);
