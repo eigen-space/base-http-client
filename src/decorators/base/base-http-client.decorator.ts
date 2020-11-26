b7cd46b215ad0c5e6ea355f72972c509bc3056c8
Danilqa <hello.boriskas@gmail.com>
2020-11-26 16:21:13 +0300

    microfix http-client: changed typo with post to dedicated methods

--- src/decorators/base/base-http-client.decorator.ts
+++ src/decorators/base/base-http-client.decorator.ts
@@ -15,15 +15,15 @@ export abstract class BaseHttpClientDecorator implements QueryProvider {
     }
 
     put<T>(url: string, props?: CommonQueryProps): Promise<T> {
-        return this.decorateMethod<T>(this.wrappedService.post.bind(this.wrappedService), url, props);
+        return this.decorateMethod<T>(this.wrappedService.put.bind(this.wrappedService), url, props);
     }
 
     patch<T>(url: string, props?: CommonQueryProps): Promise<T> {
-        return this.decorateMethod<T>(this.wrappedService.post.bind(this.wrappedService), url, props);
+        return this.decorateMethod<T>(this.wrappedService.patch.bind(this.wrappedService), url, props);
     }
 
     delete<T>(url: string, props?: CommonQueryProps): Promise<T> {
-        return this.decorateMethod<T>(this.wrappedService.post.bind(this.wrappedService), url, props);
+        return this.decorateMethod<T>(this.wrappedService.delete.bind(this.wrappedService), url, props);
     }
 
     protected abstract decorateMethod<T>(
